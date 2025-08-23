1b2a5ea9db819513689e735cf8694929d6117e72
Gyanendra <gyanuchy32@gmail.com>
2025-08-23 19:50:06 +0545

    js managed

--- JS/about.js
+++
@@ -1,5 +0,0 @@
-// Simple JavaScript for any interactive elements
-        document.addEventListener('DOMContentLoaded', function() {
-            // You can add any JavaScript functionality here if needed
-            console.log('EduSphere About Page loaded');
-        });
